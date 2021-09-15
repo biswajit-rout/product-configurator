f317527b5ee77213ff79840a1bebe516125f966e
BISWAJIT ROUT <rout.biswajit2028@gmail.com>
2021-09-15 09:33:53 +0530

    deploy files added

--- src/Store/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit"
-import productReducer from "./Product/ProductReducer";
-
-const rootReducer = combineReducers({
-    product: productReducer
-});
-
-export default rootReducer;
--- src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
-import rootReducer from "./RootReducer";
-
-const middleWare = [logger, thunk];
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWare)))
